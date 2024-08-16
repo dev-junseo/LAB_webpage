6dd1924a04f34346c75dd1a6ac219ecd17fe2d58
JS <80705329+dev-junseo@users.noreply.github.com>
2024-08-16 17:47:58 +0900

    헤더 제작

--- lab-webpage/src/components/layout/Header.tsx
+++ lab-webpage/src/components/layout/Header.tsx
@@ -1,24 +1,24 @@
 function Header() {
   return (
-    <div>
-      <div className="logo">
-        <a href="/">
-          <img src="#" alt="Logo" />
-        </a>
-      </div>
-      <nav className="nav-bar">
-        <ul className="menu">
-          <li className="menu__parent">
-            <a href="#">ABOUT</a>
-          </li>
-          <li className="menu__parent">
-            <a href="#">PROJECT</a>
-          </li>
-          <li className="menu__parent">
-            <a href="#">DEPARTMENT</a>
-          </li>
-        </ul>
-      </nav>
+    <div className="flex justify-between items-center w-full h-20 bg-none">
+      <div className="ml-12">
+            <a href="#">
+                <img className="w-20 h-18 block m-auto" alt="" src="/lab-logo.png" />
+            </a>
+        </div>
+        <nav className="">
+            <ul className="flex justify-end mr-12">
+                <li className="flex w-32 h-10 justify-center items-center">
+                    <a href="#">ABOUT</a>
+                </li>
+                <li className="flex w-32 h-10 justify-center items-center">
+                    <a href="#">PROJECT</a>
+                </li>
+                <li className="flex w-32 h-10 justify-center items-center">
+                    <a href="#">DEPARTMENT</a>
+                </li>
+            </ul>
+        </nav>
     </div>
   );
 }
--- lab-webpage/src/main/Mainpage.tsx
+++ lab-webpage/src/main/Mainpage.tsx
@@ -1,10 +1,13 @@
 function MainPage() {
   return (
     <div>
-      <div className="side-bar">
-        <div className="side-bar__comment slide-right">
-          <h1 className="main__name text-3xl font-bold">(주) Hyundai E&T</h1>
-          <h2 className="text-2xl">technical team</h2>
+      <div className="w-full h-screen relative">
+        <img className="absolute w-full h-screen" src="main_img.jpg" alt="" />
+        <div className="absolute bg-[rgba(89,120,148,0.5)] w-1/3 h-screen flex justify-end items-center">
+          <div className="side-bar__comment slide-right">
+            <h1 className="main__name text-3xl font-bold">Data Fustion Steel</h1>
+            <h1 className="main__name text-3xl font-bold">Material Design Laboratory</h1>
+          </div>
         </div>
       </div>
     </div>
