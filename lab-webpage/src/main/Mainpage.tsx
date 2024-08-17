b41be5a0cda6c09ee21bb132caf1ee516ad95e05
JS <80705329+dev-junseo@users.noreply.github.com>
2024-08-17 15:42:18 +0900

    view count 추가

--- lab-webpage/src/main/Mainpage.tsx
+++ lab-webpage/src/main/Mainpage.tsx
@@ -43,6 +43,9 @@ function MainPage() {
       <div className="w-full h-[100vh] bg-cyan-900">
         
       </div>
+      <a href="https://info.flagcounter.com/0UwV">
+       <img src="https://s11.flagcounter.com/count2/0UwV/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_1/flags_0/percent_0/" alt="Flag Counter" />
+      </a>
     </div>
   );
 }
